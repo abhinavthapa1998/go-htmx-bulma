f4b3cf248067446296251265f1b6ac841826b161
Abhinav Thapa <abhinavthapa1998@gmail.com>
2023-12-25 22:18:19 +0530

    Init

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+  "html/template"
+  "log"
+  "net/http"
+)
+
+type Film struct {
+  Title string
+  Director string
+}
 
 func main(){
-  fmt.Println("hello world")
+  h1 := func (w http.ResponseWriter, r *http.Request){
+    tmpl := template.Must(template.ParseFiles("index.html"))
+    films := map[string][]Film{
+      "Films":{
+        {Title: "The Godfather", Director: "Francis Ford Coppola"},
+        {Title: "Bladerunner", Director: "Ridley Scott"},
+        {Title: "The Thing", Director: "John Carpenter"},
+      },
+    }
+    tmpl.Execute(w, films)
+  }
+  h2 := func (w http.ResponseWriter, r *http.Request){
+    title := r.PostFormValue("title")
+    director := r.PostFormValue("director")  
+    tmpl := template.Must(template.ParseFiles("index.html"))
+    tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title, Director: director})
+  }
+
+  
+  http.HandleFunc("/", h1)
+  http.HandleFunc("/add-film/",h2)
+  log.Fatal(http.ListenAndServe(":8000",nil))
 }
