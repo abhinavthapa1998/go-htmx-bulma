635df670699ba57c9d6b4443d33711d663d166ca
Abhinav Thapa <abhinavthapa1998@gmail.com>
2023-12-26 00:18:48 +0530

    added json tags and auto-gen test

--- main.go
+++ main.go
@@ -1,39 +1,37 @@
 package main
 
 import (
-  "html/template"
-  "log"
-  "net/http"
+	"html/template"
+	"log"
+	"net/http"
 )
 
 type Film struct {
-  Title string
-  Director string
+	Title    string `json:"title"`
+	Director string `json:"director"`
 }
 
-func main(){
-  
-  getHome := func (w http.ResponseWriter, r *http.Request){
-    tmpl := template.Must(template.ParseFiles("index.html"))
-    films := map[string][]Film{
-      "Films":{
-        {Title: "The Godfather", Director: "Francis Ford Coppola"},
-        {Title: "Bladerunner", Director: "Ridley Scott"},
-        {Title: "The Thing", Director: "John Carpenter"},
-      },
-    }
-    tmpl.Execute(w, films)
-  }
+func main() {
+	getHome := func(w http.ResponseWriter, _ *http.Request) {
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		films := map[string][]Film{
+			"Films": {
+				{Title: "The Godfather", Director: "Francis Ford Coppola"},
+				{Title: "Bladerunner", Director: "Ridley Scott"},
+				{Title: "The Thing", Director: "John Carpenter"},
+			},
+		}
+		tmpl.Execute(w, films)
+	}
 
-  createFilm := func (w http.ResponseWriter, r *http.Request){
-    title := r.PostFormValue("title")
-    director := r.PostFormValue("director")  
-    tmpl := template.Must(template.ParseFiles("index.html"))
-    tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title, Director: director})
-  }
+	createFilm := func(w http.ResponseWriter, r *http.Request) {
+		title := r.PostFormValue("title")
+		director := r.PostFormValue("director")
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title, Director: director})
+	}
 
-  
-  http.HandleFunc("/", getHome)
-  http.HandleFunc("/add-film/", createFilm)
-  log.Fatal(http.ListenAndServe(":8080",nil))
+	http.HandleFunc("/", getHome)
+	http.HandleFunc("/add-film/", createFilm)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
